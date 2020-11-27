b1a098ee37f71262b03b84361ab5a3e098e51ddc
109306048 <gean12141214@gmail.com>
2020-11-27 11:06:24 +0800

    first commit

---
+++ Lab3拷貝.java
@@ -0,0 +1,41 @@
+
+public class Lab3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	int a,b;
+	double c,d;
+	a=5;
+	b=8;
+	c=5.8;
+	d=8.5;
+	
+	System.out.println("number a="+a);
+	System.out.println("number b="+b);
+	System.out.println("number c="+c);
+	System.out.println("number d="+d);
+	
+	System.out.println("number a+number b="+(a+b));
+	System.out.println("number c+number d="+(c+d));
+	
+	int number;
+	//number=12.14; 因為不能將非整數assign進integer的空間。
+	
+    double number2;
+    number2=12;// 整數可以assign進decimal variable的空間。
+	
+	int day_of_month;
+	day_of_month=30;
+	System.out.println("day_of_month:"+day_of_month);
+	
+	byte day_of_month2=(byte) day_of_month;
+		
+		
+		
+		
+		
+		
+	}
+
+}
